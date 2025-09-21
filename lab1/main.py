1d00d8d1eda4845c730d648efc54ff7d24324810
Albert <albertoganesan5@gmail.com>
2025-09-21 17:05:43 +0700

    LAB_1. Вроде должно работать?

---
+++ lab1/main.py
@@ -0,0 +1,63 @@
+import re
+
+def parse_expression(s):
+    s = s.replace(' ', '')
+    tokens = re.findall(r'\d+|[a-zA-Z][a-zA-Z0-9]*|[+*/-]|\(|\)', s) + ['&']
+    pos = [0]  
+    
+    def curr(): return tokens[pos[0]]
+    def next_token(): pos[0] += 1
+    
+    def S():
+        print('S ')
+        T()
+        E()
+    
+    def E():
+        print('E ')
+        if curr() in '+-':
+            next_token()
+            T()
+            E()
+    
+    def T():
+        print('T ')
+        if curr() in '*/':
+            next_token()
+            F()
+            T()
+        elif curr() == '(' or re.match(r'^\d+$', curr()) or re.match(r'^[a-zA-Z]', curr()):
+            F()
+            T()
+    def F():
+        print('F ')
+        if curr() == '(':
+            next_token()
+            S()
+            if curr() != ')':
+                raise Exception("Ожидалось ')'")
+            next_token()
+        elif re.match(r'^\d+$', curr()) or re.match(r'^[a-zA-Z]', curr()):
+            next_token()
+        else:
+            raise Exception("Ожидалось number, id или '('")
+    
+    try:
+        S()
+        if curr() == '&':
+            return "Выражение корректно"
+        else:
+            return "Ошибка: неожиданный конец выражения"
+    except Exception as e:
+        return f"Ошибка: {e}"
+
+# Тестирование
+test_cases = [
+
+    "5 + + 3"
+
+]
+
+for test in test_cases:
+    result = parse_expression(test)
+    print(f"'{test}' -> {result}")
